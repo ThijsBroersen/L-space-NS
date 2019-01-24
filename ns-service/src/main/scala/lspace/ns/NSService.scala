b2f1533336eedae287873646aa85a94a8af0e9d3
Thijs Broersen <mht.broersen@gmail.com>
2019-01-24 14:50:11 +0100

    initial ns-service

---
+++ ns-service/src/main/scala/lspace/ns/NSService.scala
@@ -0,0 +1,99 @@
+package lspace.ns
+
+import argonaut.{EncodeJson, Json, PrettyParams}
+import cats.effect.IO
+import com.twitter.finagle.{Http, Service}
+import com.twitter.finagle.http.{Request, Response, Status}
+import com.twitter.finagle.param.Stats
+import com.twitter.io.Buf
+import com.twitter.server.TwitterServer
+import com.twitter.util.Await
+import io.finch
+import io.finch.{Bootstrap, Endpoint, Ok, Output}
+import lspace.client.User
+import lspace.librarian.process.traversal.P
+import lspace.librarian.process.traversal.Step
+import lspace.librarian.provider.mem.MemGraph
+import lspace.librarian.structure._
+import lspace.parse.JsonLD
+import lspace.ns._
+import lspace.services.rest.endpoints.JsonLDModule
+import org.slf4j.LoggerFactory
+import shapeless.{:+:, CNil}
+
+import scala.collection.mutable
+
+object NSService extends TwitterServer {
+
+  lazy val graph: Graph = MemGraph("https://ns.l-space.eu/")
+  val context           = graph.iri
+  lazy val nsService    = NSService(context, graph)
+
+  P.predicates.map(_.ontology).foreach(graph.ns.ontologies.store)
+  Step.steps.map(_.ontology).foreach(graph.ns.ontologies.store)
+  graph.ns.ontologies.store(File.ontology)
+  graph.ns.ontologies.store(Directory.ontology)
+  graph.ns.ontologies.store(User.ontology)
+  File.properties.foreach(graph.ns.properties.store)
+  Directory.properties.foreach(graph.ns.properties.store)
+  User.properties.foreach(graph.ns.properties.store)
+
+  lazy val port: Int = 8080
+
+  import JsonLDModule.Encode._
+
+  lazy val api: Service[Request, Response] = Bootstrap
+    .configure(enableMethodNotAllowed = true, enableUnsupportedMediaType = true)
+    .serve[JsonLDModule.JsonLD :+: CNil](nsService.api)
+    .toService
+
+  def main(): Unit = {
+    val server = Http.server
+//      .configured(Stats(statsReceiver))
+      .serve(
+        s":$port",
+        api
+      )
+
+    onExit {
+      println(s"close graph-server for ${nsService.graph.iri}")
+      server.close()
+    }
+
+    Await.ready(adminHttpServer)
+  }
+}
+case class NSService(context: String, graph: Graph) extends JsonLDModule {
+  val log    = LoggerFactory.getLogger(getClass)
+  val jsonld = JsonLD(graph)
+
+  val headersAll = root.map(_.headerMap.toMap)
+  val cache      = mutable.HashMap[String, mutable.HashMap[String, Json]]()
+
+  /**
+    * retrieve a single resource
+    * @param uri
+    * @return
+    */
+  val getUri: Endpoint[IO, Json] = get(paths[String]) { (paths: Seq[String]) =>
+    val path = paths.mkString("/")
+    cache
+      .get(context + path)
+      .flatMap(_.get("application/ld+json"))
+      .orElse(
+        graph.ns.nodes
+          .hasIri(context + path)
+          .headOption
+          .map(jsonld.encode(_))
+          .map { json =>
+            cache += (context + path)                                                              -> (cache
+              .getOrElse(context + path, mutable.HashMap[String, Json]()) += "application/ld+json" -> json)
+            json
+          })
+      .map(Ok(_)) //.withHeader("Content-Type", "application/ld+json"))
+      .getOrElse(finch.NotFound(new Exception("unknown path")))
+  }
+
+  val api = getUri
+
+}
---
+++ ns-service/src/test/scala/lspace/ns/NSServiceSpec.scala
@@ -0,0 +1,37 @@
+package lspace.ns
+
+import com.twitter.finagle.http.Status
+import io.finch.Input
+import org.scalatest.{AsyncWordSpec, BeforeAndAfterAll, Matchers}
+
+class NSServiceSpec extends AsyncWordSpec with Matchers with BeforeAndAfterAll {
+
+  println(NSService.nsService.api.toString)
+
+  import util._
+  "The nsService" can {
+    "return namespace-resources in applicication/ld+json" in {
+      val input = Input
+        .get("/File/ext")
+        .withHeaders("Accept" -> "application/ld+json")
+      val res = NSService.api(input.request)
+
+      res.map { response =>
+        val headers = response.headerMap
+
+        response.status shouldBe Status.Ok
+        response.contentType shouldBe Some("application/ld+json")
+      }
+    }
+    "return UnsupportedMediaType for unsupported Accept-header" ignore {
+      val input = Input
+        .get("/File/ext")
+        .withHeaders("Accept" -> "weird/type")
+      val res = NSService.api(input.request)
+
+      res.map { response =>
+        response.status shouldBe Status.UnsupportedMediaType
+      }
+    }
+  }
+}
